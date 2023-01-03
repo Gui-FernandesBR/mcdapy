82957d5b1921d4533da464eea117bd9570c4b83e
Guilherme <guilherme_fernandes@usp.br>
2023-01-03 04:33:33 +0100

    MAINT: drop python 3.7 support

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     ],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    python_requires=">=3.7",
+    python_requires=">=3.3",
     install_requires=required,
     install_lib="mcdapy",
 )
